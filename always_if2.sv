b61948c2e1698cd14ea1e376c1847015de0fd8bf
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2022-01-30 23:48:20 +0800

    Create always_if2.sv

---
+++ always_if2.sv
@@ -0,0 +1,22 @@
+module top_module (
+    input logic cpu_overheated,
+    output logic shut_off_computer,
+    input logic arrived,
+    input logic gas_tank_empty,
+    output logic keep_driving  ); //
+
+    always @(*) begin
+        if (cpu_overheated)
+           shut_off_computer = 1;
+        else
+           shut_off_computer = 0;
+    end
+
+    always @(*) begin
+        if (~arrived)
+           keep_driving = ~gas_tank_empty;
+        else
+           keep_driving = 0; 
+    end
+
+endmodule
