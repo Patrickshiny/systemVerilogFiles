8efb05f5a44f413bf5a2737e0e720ecfee268413
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2022-01-30 23:53:48 +0800

    Create alwaysblock1.sv

---
+++ alwaysblock1.sv
@@ -0,0 +1,14 @@
+module top_module(
+    input logic a, 
+    input logic b,
+    output logic out_assign,
+    output logic out_alwaysblock
+);
+
+    assign out_assign = a & b;
+    always @ (*)
+        begin
+            out_alwaysblock = a & b;
+        end
+    
+endmodule
