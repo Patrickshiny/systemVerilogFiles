03bbe7f64ccce9195e2b091621301866acd061f4
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2022-01-30 23:18:22 +0800

    Update 7458.sv

--- 7458.sv
+++ 7458.sv
@@ -1,8 +1,8 @@
 module top_module ( 
-    input p1a, p1b, p1c, p1d, p1e, p1f,
-    output p1y,
-    input p2a, p2b, p2c, p2d,
-    output p2y );
+    input logic p1a, p1b, p1c, p1d, p1e, p1f,
+    output logic p1y,
+    input logic p2a, p2b, p2c, p2d,
+    output logic p2y );
 
     /* Method 1 */
     // assign p1y = (p1a & p1b & p1c) | (p1d & p1e & p1f);
