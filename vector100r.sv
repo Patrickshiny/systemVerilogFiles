92103fc354af41e8ff4cd045622b29bd66bb6cc7
Patrickshiny <29180434+Patrickshiny@users.noreply.github.com>
2022-01-31 14:14:00 +0800

    Update vector100r.sv

--- vector100r.sv
+++ vector100r.sv
@@ -3,7 +3,7 @@ module top_module(
     output logic[99:0] out
 );
     integer count;
-    always@(*)
+    always_comb
         begin
             for(count = 0; count <=99; count++)
                 out[count] = in[99-count];
